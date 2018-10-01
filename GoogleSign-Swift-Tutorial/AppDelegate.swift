e44b2b5e49e24dbdc4944d7d323c425495c625c1
leoiphonedev <aggarwalaman1985@gmail.com>
2018-10-02 00:56:42 +0530

    Pushing code used in the tutorial

--- GoogleSign-Swift-Tutorial/AppDelegate.swift
+++ GoogleSign-Swift-Tutorial/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GIDSignIn.sharedInstance().clientID = "498646325562-plnb3nt4vc2rnnske75f4v07fp9q9jq6.apps.googleusercontent.com"
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url as URL?,
+                                                 sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
+                                                 annotation: options[UIApplicationOpenURLOptionsKey.annotation])
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- GoogleSign-Swift-Tutorial/ViewController.swift
+++ GoogleSign-Swift-Tutorial/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
 
+    @IBOutlet weak var lblTitle:UILabel!
+    @IBOutlet weak var btnGoogleSignIn:UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        btnGoogleSignIn.addTarget(self, action: #selector(signinUserUsingGoogle(_ :)), for: .touchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func signinUserUsingGoogle(_ sender: UIButton) {
+        
+        if btnGoogleSignIn.title(for: .normal) == "Sign Out" {
+            GIDSignIn.sharedInstance().signOut()
+            lblTitle.text = ""
+            btnGoogleSignIn.setTitle("Sign In using Google", for: .normal)
+        } else {
+            GIDSignIn.sharedInstance().delegate = self
+            GIDSignIn.sharedInstance().uiDelegate = self
+            GIDSignIn.sharedInstance().signIn()
+        }
+    }
 
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+        if let error = error {
+            print("We have error sign in user == \(error.localizedDescription)")
+        } else {
+            if let gmailUser = user {
+                lblTitle.text = "You are sign in using id \(gmailUser.profile.email!)"
+                btnGoogleSignIn.setTitle("Sign Out", for: .normal)
+                
+            }
+        }
+    }
 
 }
 
